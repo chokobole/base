67155f22c3ea4dbed9a45b900629cb7ae444a16c
chokobole <chokobole33@gmail.com>
2020-01-16 21:59:41 +0900

    Remove duplicated license

--- base/socket/address_family.cc
+++ base/socket/address_family.cc
@@ -6,10 +6,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Copyright (c) 2020 The Base Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 #include "base/socket/address_family.h"
 
 #include "base/logging.h"
--- base/socket/address_family.h
+++ base/socket/address_family.h
@@ -6,10 +6,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Copyright (c) 2020 The Base Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 #ifndef BASE_SOCKET_ADDRESS_FAMILY_H_
 #define BASE_SOCKET_ADDRESS_FAMILY_H_
 
