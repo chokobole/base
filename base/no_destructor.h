c13925d9410dab0882f402fc03063ddcb8879e50
chokobole <chokobole33@gmail.com>
2020-01-21 22:51:03 +0900

    Change order of license

--- base/no_destructor.h
+++ base/no_destructor.h
@@ -1,8 +1,8 @@
-// Copyright (c) 2019 The Base Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright (c) 2019 The Base Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
